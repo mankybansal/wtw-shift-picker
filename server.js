b27289ce482ec1a54a1c53e03d7da8cffd301998
Mayank Bansal <sunny.bansal@gmail.com>
2017-07-12 03:35:38 -0500

    Debugging mode working

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var webdriver = require('selenium-webdriver');
 var browser = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'chrome'}).build();
 
-var xpath = "/html/body/div/table[1]/tbody/tr[1]/td/b";
 function automate() {
     browser.get('https://www5.whentowork.com/cgi-bin/w2wE.dll/emptradeboard?SID=848771569420C');
 
@@ -9,46 +8,52 @@ function automate() {
     browser.findElements(webdriver.By.className("s2")).then(function (elements) {
         count = elements.length;
     }).then(function () {
-        console.log("\n**********************\n" + (count - 1) + " DROPPED SHIFTS FOUND \n**********************\n");
-        for (var i = 1; i < count; i++) {
+            console.log("\n**********************\n" + (count - 1) + " DROPPED SHIFTS FOUND \n**********************\n");
 
-            getElement(i, function (myIndex, myElement) {
-                //console.log(myElement);
-                myElement.getText().then(function (text) {
-                    console.log(myIndex + ". " + text);
+            console.log("------------------------------------------------");
+
+            for (var i = 1; i < 2; i++) {
+
+                getElement(i, function (myIndex, myElement) {
+                    //console.log(myElement);
+                    myElement.getText().then(function (text) {
+                        console.log(myIndex + ". " + text);
+                    });
+
+                    myElement.click();
+
+                    browser.getAllWindowHandles().then(function (handles) {
+                        browser.switchTo().window(handles[1]).then(function () {
+                            browser.findElement(webdriver.By.className("titlebox")).getText().then(function (text) {
+                                console.log("   " + text);
+                                console.log("------------------------------------------------");
+
+                                var xpath = "/html/body/div[1]/table[2]/tbody/tr[2]/td/b/a";
+
+                                browser.findElement(webdriver.By.xpath(xpath)).then(function (confirmElement) {
+                                    confirmElement.click();
+
+                                    browser.findElement(webdriver.By.className("btn-danger")).then(function (confirmSureElement) {
+//TODO: IF DON'T WANT SHIFT CANCEL AND CLOSE
+
+                                        confirmSureElement.click();
+                                        browser.getAllWindowHandles().then(function (handles) {
+                                            //console.log(handles);
+                                            browser.switchTo().window(handles[0]);
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+
                 });
-
-                myElement.click();
-
-                // browser.getAllWindowHandles().then(function (handles) {
-                //     console.log(handles);
-                //     browser.switchTo().window(handles[1]).then(function () {
-                //         browser.close();
-                //     });
-                // });
-            });
-
-
-            /*
-
-             myElement.click();
-
-             browser.getAllWindowHandles().then(function (handles) {
-             console.log(handles);
-             browser.switchTo().window(handles[1]).then(function () {
-             browser.close();
-             });
-             });*/
+            }
+            //setTimeout(automate, 3500);
         }
-    });
+    );
 
-
-    /*.then(function () {
-     browser.getAllWindowHandles().then(function (handles) {
-     console.log(handles);
-
-     browser.switchTo().window(handles[1]);
-     /!*
+    /*
 
      browser.findElement(webdriver.By.xpath(xpath)).getText().then(function (text) {
      console.log(" ---- " + text);
@@ -69,4 +74,4 @@ function getElement(sourceIndex, callback) {
     })
 }
 automate();
-//setInterval(automate, 5000);
+//setInterval(automate, 2000);
