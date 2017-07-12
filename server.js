84d850707b256a5b0500f1bc0e262f9cfdb9b871
Mayank Bansal <sunny.bansal@gmail.com>
2017-07-12 03:04:53 -0500

    Shifts found

--- server.js
+++ server.js
@@ -1,15 +1,72 @@
 var webdriver = require('selenium-webdriver');
 var browser = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'chrome'}).build();
 
-
+var xpath = "/html/body/div/table[1]/tbody/tr[1]/td/b";
 function automate() {
     browser.get('https://www5.whentowork.com/cgi-bin/w2wE.dll/emptradeboard?SID=848771569420C');
-    browser.findElements(webdriver.By.className("s2")).then(function(elements){
-        elements.forEach(function (element) {
-            element.getText().then(function(text){
-                console.log(text);
+
+    var count = 0;
+    browser.findElements(webdriver.By.className("s2")).then(function (elements) {
+        count = elements.length;
+    }).then(function () {
+        console.log("\n**********************\n" + (count - 1) + " DROPPED SHIFTS FOUND \n**********************\n");
+        for (var i = 1; i < count; i++) {
+
+            getElement(i, function (myIndex, myElement) {
+                //console.log(myElement);
+                myElement.getText().then(function (text) {
+                    console.log(myIndex + ". " + text);
+                });
+
+                myElement.click();
+
+                // browser.getAllWindowHandles().then(function (handles) {
+                //     console.log(handles);
+                //     browser.switchTo().window(handles[1]).then(function () {
+                //         browser.close();
+                //     });
+                // });
             });
-        });
+
+
+            /*
+
+             myElement.click();
+
+             browser.getAllWindowHandles().then(function (handles) {
+             console.log(handles);
+             browser.switchTo().window(handles[1]).then(function () {
+             browser.close();
+             });
+             });*/
+        }
     });
+
+
+    /*.then(function () {
+     browser.getAllWindowHandles().then(function (handles) {
+     console.log(handles);
+
+     browser.switchTo().window(handles[1]);
+     /!*
+
+     browser.findElement(webdriver.By.xpath(xpath)).getText().then(function (text) {
+     console.log(" ---- " + text);
+     });*!/
+
+
+     console.log(browser.getPageSource());
+     });
+     });*/
 }
-setInterval(automate, 5000);
+
+function getElement(sourceIndex, callback) {
+    browser.findElements(webdriver.By.className("s2")).then(function (elements) {
+        elements.forEach(function (value, index) {
+            if (index == sourceIndex)
+                return callback(sourceIndex, value);
+        });
+    })
+}
+automate();
+//setInterval(automate, 5000);
