946733704823ee235313ca968de59e8237e91a2e
Mayank Bansal <sunny.bansal@gmail.com>
2017-07-13 15:58:44 -0500

    Added Trade or Drop picker

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var webdriver, browser, confirmClass,
-    baseURL = "https://www5.whentowork.com/cgi-bin/w2wE.dll/emptradeboard?SID=1697883600420C&Date=08/13/2017";
+    //baseURL = "https://www5.whentowork.com/cgi-bin/w2wE.dll/emptradeboard?SID=1697883600420C&Date=07/09/2017";
+    baseURL = "https://www5.whentowork.com/cgi-bin/w2wE.dll/emptradeboard?SID=1535014173420C&Date=08/13/2017";
 var args = process.argv.slice(2);
 
 console.log("\x1b[33m%s\x1b[0m", "\n----------------------------\nWhenToWork Shift Picking Bot\n----------------------------");
-console.log("Version 1.0 Beta");
+console.log("Version 1.1 Beta");
 
 if (args[2] == "-live") {
     console.log("\x1b[35m%s\x1b[0m", "*** Running in LIVE mode. ***");
@@ -20,7 +21,6 @@ if (args[0] == "-url") {
 
     webdriver = require('selenium-webdriver');
     browser = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'chrome'}).build();
-
 } else {
     console.log('\x1b[31m%s\x1b[0m', "\nERROR: Must specify base URL\n");
     process.exit()
@@ -30,24 +30,33 @@ console.reset = function () {
     return process.stdout.write('\033c');
 };
 
+process.on('SIGINT', function() {
+    console.log( "\nStopping wtw-shift-picker" );
+    browser.close();
+
+    console.log( "Browser Closed. Exiting..." );
+    // some other closing procedures go here
+    process.exit( );
+});
+
 function automate() {
 
     browser.get(baseURL);
     var count = 0;
 
-    browser.findElements(webdriver.By.className("s2")).then(function (elements) {
+    browser.findElements(webdriver.By.css(".s2, .s3")).then(function (elements) {
         count = elements.length;
     }).then(function () {
-        console.log("\x1b[32m","\n**********************\n" + (count - 1) + " DROPPED SHIFTS FOUND \n**********************");
+        console.log("\x1b[32m","\n**********************\n" + (count-2) + " DROPPED SHIFTS FOUND \n**********************");
         console.log("\x1b[0m");
 
-        if (count > 1) {
+        if (count >2) {
             console.log("------------------------------------------------");
-            for (var i = 1; i < count; i++) {
-                getElement(i, function (myIndex, myElement) {
+            for (var i = 2; i < count; i++) {
+                getElement(browser, i, function (myIndex, myElement) {
                     //console.log(myElement);
                     myElement.getText().then(function (text) {
-                        console.log(myIndex + ". " + text);
+                        console.log((myIndex-1) + ". " + text);
                     });
 
                     myElement.click();
@@ -80,12 +89,12 @@ function automate() {
             }
         }
 
-        setTimeout(automate, 3500);
+        setTimeout(automate, 10000);
     });
 }
 
-function getElement(sourceIndex, callback) {
-    browser.findElements(webdriver.By.className("s2")).then(function (elements) {
+function getElement(browser, sourceIndex, callback) {
+    browser.findElements(webdriver.By.css(".s2, .s3")).then(function (elements) {
         elements.forEach(function (value, index) {
             if (index == sourceIndex)
                 return callback(sourceIndex, value);
